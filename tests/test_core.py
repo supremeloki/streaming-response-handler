1da76009deb3bb85640d502f79321d79943cf590
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-02-26 19:01:46 +0330

    document error hierarchy

--- tests/test_core.py
+++ tests/test_core.py
@@ -106,3 +106,5 @@ def test_interrupt_raises_with_partial_data():
 def test_throughput_stats_positive():
     handler = StreamHandler(clock=FakeClock())
     _, stats = handler.consume(list(chunk_text("word " * 20)) + [done_chunk()])
+    assert stats.chunks_per_second > 0
+    assert stats.chars_per_second > 0
